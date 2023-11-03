56b404b6c577acaecddba416e41f201f58925228
golmschenk <greg@olmschenk.com>
2023-11-03 13:55:53 -0400

    Switch which binary is being used in the current triple lens test

--- tests/end_to_end_tests/triple_lens/advanced_dseek/test_advanced_dseek.py
+++ tests/end_to_end_tests/triple_lens/advanced_dseek/test_advanced_dseek.py
@@ -7,6 +7,6 @@ from tests.end_to_end_tests.run_directory_manipulations import run_trial_with_ou
 
 @pytest.mark.slow
 def test_advanced_dseek():
-    executable_path_from_root = Path('reference/minuit_torbtpar_rvgMagsclast1/minuit_torbtpar_rvgMagsclast1.xO')
+    executable_path_from_root = Path('minuit_torbtpar_rvgMagsclast1/minuit_torbtpar_rvgMagsclast1.xO')
     executable_path = Path(__file__).parent.parent.parent.parent.parent.joinpath(executable_path_from_root)
     run_trial_with_output_file_cleaning(__file__, executable_path=executable_path)
