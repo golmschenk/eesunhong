cb281e89d02a2dda3bef71aa6b0a1d740752d475
golmschenk <greg@olmschenk.com>
2024-01-06 19:28:54 -0500

    Remove message printing so it doesn't break tests

--- src/eesunhong/python_module.py
+++ src/eesunhong/python_module.py
@@ -1,5 +1,3 @@
 def multiply(a, b):
-    print('Message from Python')
     product = a * b
-    print(f'Product in Python is {product}')
     return product
--- src/main.f
+++ src/main.f
@@ -1,7 +1,6 @@
        use eesunhong_astropy_interface,
      &     only : outer_multiply
        EXTERNAL FCN
-       write(*, *) "Message from Fortran."
-       write(*, *) "Product in Fortran:", outer_multiply(4.5, 3.0)
+       call outer_multiply(4.5, 3.0)
        CALL MINUIT(FCN,0)
        END
