9a59a4f78f5c9dfc5e992403da6995600ebcd3b3
golmschenk <greg@olmschenk.com>
2024-01-10 20:57:33 -0500

    Add VBBL call to calculate parallax, but it's getting a very different number from geo_par

--- src/eesunhong/eesunhong.py
+++ src/eesunhong/eesunhong.py
@@ -1,6 +1,6 @@
 import errno
 import os
-from ctypes import CDLL, POINTER, c_int, c_double
+from ctypes import CDLL, POINTER, c_int, c_double, c_void_p, c_char_p
 from pathlib import Path
 import platform
 
@@ -12,25 +12,25 @@ winmode = None
 
 platform_system_name = platform.system()
 if platform_system_name == 'Darwin':
-    library_path = library_directory.joinpath('libeesunhong_fortran_library.dylib')
+    eesunhong_fortran_library_path = library_directory.joinpath('libeesunhong_fortran_library.dylib')
 elif platform_system_name == 'Linux':
-    library_path = library_directory.joinpath('libeesunhong_fortran_library.so')
+    eesunhong_fortran_library_path = library_directory.joinpath('libeesunhong_fortran_library.so')
 elif platform_system_name == 'Windows':
     winmode = 0
     os.add_dll_directory(f'{library_directory.absolute()}')
     os.add_dll_directory(f'{library_directory.absolute()}/lib')
     os.add_dll_directory(f'{library_directory.absolute()}/eesunhong.libs')
-    library_path = library_directory.joinpath('eesunhong_fortran_library.dll')
-    if not library_path.exists():
-        library_path = library_directory.joinpath('libeesunhong_fortran_library.dll')
+    eesunhong_fortran_library_path = library_directory.joinpath('eesunhong_fortran_library.dll')
+    if not eesunhong_fortran_library_path.exists():
+        eesunhong_fortran_library_path = library_directory.joinpath('libeesunhong_fortran_library.dll')
 else:
     raise ValueError(f'Platform system {platform_system_name} is not supported.')
 
-if not library_path.exists():
-    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), str(library_path.absolute()))
-fortran_library = CDLL(str(library_path), winmode=winmode)
+if not eesunhong_fortran_library_path.exists():
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), str(eesunhong_fortran_library_path.absolute()))
+eesunhong_fortran_library = CDLL(str(eesunhong_fortran_library_path), winmode=winmode)
 
-sort_light_curve_data_by_time = fortran_library.sort_light_curve_data_by_time
+sort_light_curve_data_by_time = eesunhong_fortran_library.sort_light_curve_data_by_time
 sort_light_curve_data_by_time.argtypes = [POINTER(c_int),
                                           POINTER(c_double),
                                           POINTER(c_double),
@@ -38,10 +38,22 @@ sort_light_curve_data_by_time.argtypes = [POINTER(c_int),
                                           POINTER(c_int),
                                           POINTER(c_int)]
 
-compute_parallax_using_geo_par = fortran_library.geo_par
+compute_parallax_using_geo_par = eesunhong_fortran_library.geo_par
 compute_parallax_using_geo_par.argtypes = [POINTER(c_double),
                                            POINTER(c_double),
                                            POINTER(c_double),
                                            POINTER(c_double),
                                            POINTER(c_double),
                                            POINTER(c_double)]
+
+create_vbbl = eesunhong_fortran_library.create_vbbl
+create_vbbl.restype = c_void_p
+
+destroy_vbbl = eesunhong_fortran_library.destroy_vbbl
+destroy_vbbl.argtypes = [c_void_p]
+
+set_object_coordinates_for_vbbl = eesunhong_fortran_library.set_object_coordinates_for_vbbl
+set_object_coordinates_for_vbbl.argtypes = [c_void_p, c_char_p, c_char_p]
+
+compute_parallax_for_vbbl = eesunhong_fortran_library.compute_parallax_for_vbbl
+compute_parallax_for_vbbl.argtypes = [c_void_p, c_double, c_double, POINTER(c_double)]
--- tests/unit_tests/test_parallax_computation.py
+++ tests/unit_tests/test_parallax_computation.py
@@ -1,54 +1,85 @@
 from ctypes import POINTER, c_double
+from pathlib import Path
 
 import numpy as np
 import pytest
 
-from eesunhong.eesunhong import compute_parallax_using_geo_par
+from eesunhong.eesunhong import compute_parallax_using_geo_par, create_vbbl, destroy_vbbl, \
+    set_object_coordinates_for_vbbl, compute_parallax_for_vbbl
 
 
-def test_parallax_computation():
-    #  t   9033.2148899999447      a   268.43250000000000      d  -32.589311111111115      f   9100.8972159999994
-    #  qn   5.1377649825789830E-002 qe  0.52891219662449951
+def test_parallax_computation_using_geo_par():
+    qn_case0 = np.array(np.nan, dtype=c_double)
+    qe_case0 = np.array(np.nan, dtype=c_double)
+    t_case0 = np.array(9033.2148899999447, dtype=c_double)
+    alpha_case0 = np.array(268.43250000000000, dtype=c_double)
+    delta_case0 = np.array(-32.589311111111115, dtype=c_double)
+    tfix_case0 = np.array(9100.8972159999994, dtype=c_double)
 
-    #  t   9100.8258499999993      a   268.43250000000000      d  -32.589311111111115      f   9100.8972159999994
-    #  qn   8.6430833360821114E-009 qe   7.3889854740073971E-007
+    compute_parallax_using_geo_par(qn_case0.ctypes.data_as(POINTER(c_double)),
+                                   qe_case0.ctypes.data_as(POINTER(c_double)),
+                                   t_case0.ctypes.data_as(POINTER(c_double)),
+                                   alpha_case0.ctypes.data_as(POINTER(c_double)),
+                                   delta_case0.ctypes.data_as(POINTER(c_double)),
+                                   tfix_case0.ctypes.data_as(POINTER(c_double)))
 
-    qn0 = np.array(np.nan, dtype=c_double)
-    qe0 = np.array(np.nan, dtype=c_double)
-    t0 = np.array(9033.2148899999447, dtype=c_double)
-    alpha0 = np.array(268.43250000000000, dtype=c_double)
-    delta0 = np.array(-32.589311111111115, dtype=c_double)
-    tfix0 = np.array(9100.8972159999994, dtype=c_double)
+    expected_qn_case0 = np.array(5.1377649825789830e-2, dtype=c_double)
+    expected_qe_case0 = np.array(0.52891219662449951, dtype=c_double)
 
-    compute_parallax_using_geo_par(qn0.ctypes.data_as(POINTER(c_double)),
-                                   qe0.ctypes.data_as(POINTER(c_double)),
-                                   t0.ctypes.data_as(POINTER(c_double)),
-                                   alpha0.ctypes.data_as(POINTER(c_double)),
-                                   delta0.ctypes.data_as(POINTER(c_double)),
-                                   tfix0.ctypes.data_as(POINTER(c_double)))
+    assert qe_case0 == pytest.approx(expected_qe_case0)
+    assert qn_case0 == pytest.approx(expected_qn_case0)
 
-    expected_qn0 = np.array(5.1377649825789830e-2, dtype=c_double)
-    expected_qe0 = np.array(0.52891219662449951, dtype=c_double)
+    qn_case1 = np.array(np.nan, dtype=c_double)
+    qe_case1 = np.array(np.nan, dtype=c_double)
+    t_case1 = np.array(9100.8258499999993, dtype=c_double)
+    alpha_case1 = np.array(268.43250000000000, dtype=c_double)
+    delta_case1 = np.array(-32.589311111111115, dtype=c_double)
+    tfix_case1 = np.array(9100.8972159999994, dtype=c_double)
 
-    assert qe0 == pytest.approx(expected_qe0)
-    assert qn0 == pytest.approx(expected_qn0)
+    compute_parallax_using_geo_par(qn_case1.ctypes.data_as(POINTER(c_double)),
+                                   qe_case1.ctypes.data_as(POINTER(c_double)),
+                                   t_case1.ctypes.data_as(POINTER(c_double)),
+                                   alpha_case1.ctypes.data_as(POINTER(c_double)),
+                                   delta_case1.ctypes.data_as(POINTER(c_double)),
+                                   tfix_case1.ctypes.data_as(POINTER(c_double)))
 
-    qn1 = np.array(np.nan, dtype=c_double)
-    qe1 = np.array(np.nan, dtype=c_double)
-    t1 = np.array(9100.8258499999993, dtype=c_double)
-    alpha1 = np.array(268.43250000000000, dtype=c_double)
-    delta1 = np.array(-32.589311111111115, dtype=c_double)
-    tfix1 = np.array(9100.8972159999994, dtype=c_double)
+    expected_qn_case1 = np.array(8.6430833360821114e-9, dtype=c_double)
+    expected_qe_case1 = np.array(7.3889854740073971E-7, dtype=c_double)
 
-    compute_parallax_using_geo_par(qn1.ctypes.data_as(POINTER(c_double)),
-                                   qe1.ctypes.data_as(POINTER(c_double)),
-                                   t1.ctypes.data_as(POINTER(c_double)),
-                                   alpha1.ctypes.data_as(POINTER(c_double)),
-                                   delta1.ctypes.data_as(POINTER(c_double)),
-                                   tfix1.ctypes.data_as(POINTER(c_double)))
+    assert qe_case1 == pytest.approx(expected_qe_case1)
+    assert qn_case1 == pytest.approx(expected_qn_case1)
 
-    expected_qn1 = np.array(8.6430833360821114e-9, dtype=c_double)
-    expected_qe1 = np.array(7.3889854740073971E-7, dtype=c_double)
 
-    assert qe1 == pytest.approx(expected_qe1)
-    assert qn1 == pytest.approx(expected_qn1)
+def test_parallax_computation_using_vbbl():
+    vbbl = create_vbbl()
+    coordinates_file_path = Path(__file__).parent.joinpath('test_parallax_computation_resources/coordinates.txt'
+                                                           ).absolute()
+    set_object_coordinates_for_vbbl(vbbl, str(coordinates_file_path).encode('utf-8'), '.'.encode('utf-8'))
+
+    t_case0 = np.array(9033.2148899999447, dtype=c_double)
+    t0_case0 = np.array(9100.8972159999994, dtype=c_double)
+    et_case0 = np.array([np.nan, np.nan], dtype=c_double)
+
+    compute_parallax_for_vbbl(vbbl,
+                              t_case0,
+                              t0_case0,
+                              et_case0.ctypes.data_as(POINTER(c_double)))
+
+    expected_et_case0 = np.array([5.1377649825789830e-2, 0.52891219662449951], dtype=c_double)
+
+    assert np.allclose(et_case0, expected_et_case0)
+
+    t_case1 = np.array(9100.8258499999993, dtype=c_double)
+    t0_case1 = np.array(9100.8972159999994, dtype=c_double)
+    et_case1 = np.array([np.nan, np.nan], dtype=c_double)
+
+    compute_parallax_for_vbbl(vbbl,
+                              t_case1,
+                              t0_case1,
+                              et_case1.ctypes.data_as(POINTER(c_double)))
+
+    expected_et_case1 = np.array([5.1377649825789830e-2, 0.52891219662449951], dtype=c_double)
+
+    assert np.allclose(et_case1, expected_et_case1)
+
+    destroy_vbbl(vbbl)
